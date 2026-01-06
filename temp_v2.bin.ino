7dee9c3ca2ce91a52b1f27910fe53123b38f6e6b
Sakreshwari <sakreshwari07@gmail.com>
2026-01-06 11:05:14 +0530

    Add files via upload

---
+++ temp_v2.bin.ino
@@ -0,0 +1,18 @@
+#include <WiFi.h>
+
+#define CURRENT_VERSION 2   // NEW VERSION
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("TEMP CODE V2 (UPDATED OVER WIFI)");
+  Serial.print("Running version: ");
+  Serial.println(CURRENT_VERSION);
+}
+
+void loop() {
+  float temp = 30.0;
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.println(" C");
+  delay(2000);
+}
